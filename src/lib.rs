8db95b8daaf281935475f9065d92f62487c33601
Alonso <jalonsomendoza32@gmail.com>
2021-07-03 17:49:09 -0500

    Problem traits fully implemented to system

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,6 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
-
 use std::collections::HashMap;
 
 use pyo3::prelude::*;
@@ -26,14 +25,12 @@ use system::System;
 
 use optimization::TrustNCG;
 
-
 #[pymodule]
 fn solver(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(build_constraints))?;
 
     Ok(())
 }
-
 
 /// Set-up the constraints functions
 ///
@@ -45,13 +42,15 @@ fn solver(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
 ///     constraint will be omitted in this map (if a lock constraint does not
 ///     lock the x-axis, then it will not be included in constraint_parameters)
 #[pyfunction]
-fn build_constraints(objects: HashMap<&str, HashMap<&str, f64>>,
-                     constraint_names: HashMap<&str, HashMap<&str, &str>>,
-                     constraint_parameters: HashMap<&str, HashMap<&str, f64>>) {
+fn build_constraints(
+    objects: HashMap<&str, HashMap<&str, f64>>,
+    constraint_names: HashMap<&str, HashMap<&str, &str>>,
+    constraint_parameters: HashMap<&str, HashMap<&str, f64>>,
+) {
     // Here we store the system information.
     let mut system = System::new();
 
-     for (c, object_names) in &constraint_names {
+    for (c, object_names) in &constraint_names {
         if c.contains("FixBase") {
             let obj_name = object_names.get("Object").unwrap();
             let ref_name = object_names.get("Reference").unwrap();
@@ -69,27 +68,27 @@ fn build_constraints(objects: HashMap<&str, HashMap<&str, f64>>,
 
             // Finally, add the fix constraint. Note that a Fix constraint is
             // broken into fix base and fix rotation
-            let fix_base_constraint = constraints::FixBaseConstraint::new(
-                &mut system, obj_name, ref_name, c_params
-            );
-            system.constraints.push(ConstraintType::FixBaseConstraint(fix_base_constraint));
+            let fix_base_constraint =
+                constraints::FixBaseConstraint::new(&mut system, obj_name, ref_name, c_params);
+            system
+                .constraints
+                .push(ConstraintType::FixBaseConstraint(fix_base_constraint));
         }
         // TODO: make a fix_rotation_constraint
         // TODO: make a lock_constraint
         // TODO: make a equality_constraint
-     }
+    }
 
     // Un-comment this part in order to solve the problem (it is faster than the
     // implementation in python
-//     system.add_indices();
-//     let x0 = system.start_position();
-//
-//     let mut min = TrustNCG::new();
-//
-//     let sol = min.minimize(&x0, &mut system);
-//
-//     println!("Solution succeeded?: {}, iterations: {}, function evaluations: {}, \
-//     gradient evaluations: {}", sol.success, sol.iter_num, sol.f_evals, sol.f_grad_evals);
-//     println!("solution x: {}", sol.x);
+    //     system.add_indices();
+    //     let x0 = system.start_position();
+    //
+    //     let mut min = TrustNCG::new();
+    //
+    //     let sol = min.minimize(&x0, &mut system);
+    //
+    //     println!("Solution succeeded?: {}, iterations: {}, function evaluations: {}, \
+    //     gradient evaluations: {}", sol.success, sol.iter_num, sol.f_evals, sol.f_grad_evals);
+    //     println!("solution x: {}", sol.x);
 }
-
